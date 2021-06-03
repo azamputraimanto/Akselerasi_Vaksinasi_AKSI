76063d38326c3d5b5890fed7fea175c8d31b1ee1
Azamputraimanto <Vectraland15@gmail.com>
2021-06-03 10:22:20 +0700

    implementation feature get patient data

--- app/src/main/java/com/capstone/project/akselerasi_vaksinasi/ui/home/HomeFragment.kt
+++ app/src/main/java/com/capstone/project/akselerasi_vaksinasi/ui/home/HomeFragment.kt
@@ -9,15 +9,28 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.capstone.project.akselerasi_vaksinasi.R
+import com.capstone.project.akselerasi_vaksinasi.model.Patient
+import com.capstone.project.akselerasi_vaksinasi.ui.detail.DetailPatientActivity
+import com.capstone.project.akselerasi_vaksinasi.ui.home.adapter.HomeAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.firebase.ui.database.FirebaseRecyclerOptions.Builder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var auth: FirebaseAuth
+
+    private lateinit var homeAdapter: HomeAdapter
+
+    private lateinit var database: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +41,7 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.tvWelcome)
+        val recyclerViewMain: RecyclerView = root.findViewById(R.id.rvMain)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
@@ -39,6 +53,31 @@ class HomeFragment : Fragment() {
             Log.d("user", "user sign out!")
         }
         // --- sampai sini
+
+        database = FirebaseDatabase.getInstance().getReference("patients")
+        val options: FirebaseRecyclerOptions<Patient> = Builder<Patient>()
+            .setQuery(database, Patient::class.java)
+            .build()
+        homeAdapter = HomeAdapter(options, ::onItemClick)
+        recyclerViewMain.apply {
+            layoutManager = LinearLayoutManager(root.context)
+            adapter = homeAdapter
+        }
+
         return root
+    }
+
+    private fun onItemClick(patient: Patient) {
+        DetailPatientActivity.start(requireContext(), patient)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        homeAdapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        homeAdapter.stopListening()
     }
 }
